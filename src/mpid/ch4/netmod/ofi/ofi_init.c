7f491984f9bb16ebd98d47edd1023d4e14c52f5d
Rohit Zambre <rzambre@uci.edu>
2019-06-04 18:48:03 +0000

    ch4/ofi: Changes for successful compilation of domain-per-VNI

--- src/mpid/ch4/netmod/ofi/ofi_init.c
+++ src/mpid/ch4/netmod/ofi/ofi_init.c
@@ -755,6 +755,7 @@ int MPIDI_OFI_mpi_init_hook(int rank, int size, int appnum, int *tag_bits, MPIR_
                         &av_attr,       /* In:  Configuration object  */
                         &MPIDI_OFI_global.av,   /* Out: AV Object             */
                         NULL)) {        /* Context: AV events         */
+        printf("Not supported yet for multiple VNIs\n");
         do_av_insert = 0;
 
         /* TODO - the copy from the pre-existing av map into the 'MPIDI_OFI_AV' */
@@ -762,7 +763,7 @@ int MPIDI_OFI_mpi_init_hook(int rank, int size, int appnum, int *tag_bits, MPIR_
         /* directly references the mapped fi_addr_t array instead               */
         mapped_table = (fi_addr_t *) av_attr.map_addr;
         for (i = 0; i < size; i++) {
-            MPIDI_OFI_AV(&MPIDIU_get_av(0, i)).dest = mapped_table[i];
+            MPIDI_OFI_AV(&MPIDIU_get_av(0, i)).dest[0 /*WRONG*/] = mapped_table[i];
 #if MPIDI_OFI_ENABLE_RUNTIME_CHECKS
             MPIDI_OFI_AV(&MPIDIU_get_av(0, i)).ep_idx = 0;
 #else
@@ -882,7 +883,7 @@ int MPIDI_OFI_mpi_init_hook(int rank, int size, int appnum, int *tag_bits, MPIR_
                            avmap);
 
             for (i = 0; i < num_nodes; i++) {
-                MPIDI_OFI_AV(&MPIDIU_get_av(0, node_roots[i])).dest = mapped_table[i];
+                MPIDI_OFI_AV(&MPIDIU_get_av(0, node_roots[i])).dest[0 /*WRONG*/] = mapped_table[i];
 #if MPIDI_OFI_ENABLE_RUNTIME_CHECKS
                 MPIDI_OFI_AV(&MPIDIU_get_av(0, node_roots[i])).ep_idx = 0;
 #else
@@ -1184,6 +1185,7 @@ int MPIDI_OFI_get_local_upids(MPIR_Comm * comm, size_t ** local_upid_size, char 
 int MPIDI_OFI_upids_to_lupids(int size, size_t * remote_upid_size, char *remote_upids,
                               int **remote_lupids)
 {
+    printf("Not supported for multiple VNIs\n");
     int i, mpi_errno = MPI_SUCCESS;
     int *new_avt_procs;
     char **new_upids;
@@ -1235,8 +1237,8 @@ int MPIDI_OFI_upids_to_lupids(int size, size_t * remote_upid_size, char *remote_
             MPIDI_OFI_CALL(fi_av_insert(MPIDI_OFI_global.av, new_upids[i],
                                         1,
                                         (fi_addr_t *) &
-                                        MPIDI_OFI_AV(&MPIDIU_get_av(avtid, i)).dest, 0ULL,
-                                        NULL), avmap);
+                                        MPIDI_OFI_AV(&MPIDIU_get_av(avtid, i)).dest[0 /*WRONG*/],
+                                        0ULL, NULL), avmap);
 #if MPIDI_OFI_ENABLE_RUNTIME_CHECKS
             MPIDI_OFI_AV(&MPIDIU_get_av(avtid, i)).ep_idx = 0;
 #else
@@ -1246,7 +1248,7 @@ int MPIDI_OFI_upids_to_lupids(int size, size_t * remote_upid_size, char *remote_
 #endif
             MPL_DBG_MSG_FMT(MPIDI_CH4_DBG_MAP, VERBOSE,
                             (MPL_DBG_FDEST, "\tupids to lupids avtid %d lpid %d mapped to %" PRIu64,
-                             avtid, i, MPIDI_OFI_AV(&MPIDIU_get_av(avtid, i)).dest));
+                             avtid, i, MPIDI_OFI_AV(&MPIDIU_get_av(avtid, i)).dest[0 /*WRONG*/]));
             /* highest bit is marked as 1 to indicate this is a new process */
             (*remote_lupids)[new_avt_procs[i]] = MPIDIU_LUPID_CREATE(avtid, i);
             MPIDIU_LUPID_SET_NEW_AVT_MARK((*remote_lupids)[new_avt_procs[i]]);
