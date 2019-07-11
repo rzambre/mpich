b6e25a27d88dcf0d16aa9e662f87e80343480e3f
Rohit Zambre <rzambre@uci.edu>
2019-07-11 22:38:07 +0000

    ch4: Banner to show poking of global progress

--- src/mpid/ch4/src/ch4_impl.h
+++ src/mpid/ch4/src/ch4_impl.h
@@ -893,6 +893,7 @@ MPL_STATIC_INLINE_PREFIX int MPIDIG_wait_am_acc(MPIR_Win * win, int target_rank,
         MPIDIG_win_target_t *target_ptr = MPIDIG_win_target_find(win, target_rank);
         while ((target_ptr && MPIR_cc_get(target_ptr->remote_acc_cmpl_cnts) != 0) ||
                MPIR_cc_get(MPIDIG_WIN(win, remote_acc_cmpl_cnts)) != 0) {
+            printf("Calling global progress\n");
             MPIDIU_PROGRESS();
         }
     }
